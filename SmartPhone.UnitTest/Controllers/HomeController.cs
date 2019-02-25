61214bf5fa16d2f7ccd79ed4d0a3e16e9d0ee604
huy <hoanghuy97cntt@gmail.com>
2019-02-26 00:23:02 +0700

    Thêm Model Domain entity cho project

--- SmartPhone.UnitTest/Controllers/HomeController.cs
+++ SmartPhone.UnitTest/Controllers/HomeController.cs
@@ -16,7 +16,6 @@ namespace SmartPhone.UnitTest.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
