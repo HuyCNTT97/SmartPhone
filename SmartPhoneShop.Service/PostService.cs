d8d10f231d5158115f05936864d3a1cb6c69d60b
huy <hoanghuy97cntt@gmail.com>
2019-02-26 15:21:18 +0700

    Commit trước khi tạo

---
+++ SmartPhoneShop.Service/PostService.cs
@@ -0,0 +1,30 @@
+﻿using SmartPhoneShop.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPhoneShop.Service
+{
+    public interface IPostService
+    {
+        void Add(Post post);
+
+        void Update(Post post);
+
+        void Delete(int id);
+
+        IEnumerable<Post> GetAll();
+
+        IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow);
+
+        Post GetByID(int id);
+
+        IEnumerable<Post> GetAllTagPaging(int page, int pageSize, out int totalRow);
+    }
+
+    internal class PostService
+    {
+    }
+}
